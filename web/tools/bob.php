36dd14b6c2520ab8527210536e071c52cbb36175
opsgriller <zachst@hotmail.com>
2020-06-09 04:44:31 +0000

    add strlen command

---
+++ web/tools/bob.php
@@ -0,0 +1,13 @@
+
+<?php
+	print "<br /><br />";
+        print "&nbsp;~^~~~~<br />";
+        print "&nbsp;&nbsp;/&nbsp;~&nbsp;&nbsp;&nbsp;~&nbsp;\\<br />";
+        print "&nbsp;(&nbsp;@&nbsp;&nbsp;@&nbsp;)<br />";
+        print "&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;{}&nbsp;&nbsp;&nbsp;|<br /> ";
+        print "&nbsp;&nbsp;|&nbsp;^==^&nbsp;|<br /> ";
+        print "&nbsp;&nbsp;&nbsp;\------/<br />";
+        print "&nbsp;&nbsp;o--|--o<br />";
+        print "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;|<br /> ";
+        print "&nbsp;&nbsp;&nbsp;&nbsp;_|&nbsp;|_<br />";
+?>
